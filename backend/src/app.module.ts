1d27849efcdd61ab6d66c93a64b3ad9783b721bd
Anna <zharitonchik@icloud.com>
2026-06-29 13:09:36 +0300

    feat(attribute): create entity attribute
    handle http requests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AttributeModule } from './attribute/attribute.module';
 
 @Module({
-  imports: [],
+  imports: [AttributeModule],
   controllers: [AppController],
   providers: [AppService],
 })
