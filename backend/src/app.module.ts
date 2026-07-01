033f4daaa3aa2165d7d9c13ab85c23caff003f0f
Anna <zharitonchik@icloud.com>
2026-07-01 16:49:27 +0300

    refactor(prisma): add prisma entity
    set up working with the database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AttributeModule } from './attribute/attribute.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AttributeModule],
+  imports: [PrismaModule, AttributeModule],
   controllers: [AppController],
   providers: [AppService],
 })
