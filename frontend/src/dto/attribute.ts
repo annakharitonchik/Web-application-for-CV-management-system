f0a60f1ea1fd02e26fb8df355d1d107e14b7b87b
Anna <zharitonchik@icloud.com>
2026-07-03 02:08:29 +0300

    feat(attribute.ts): create class AttributeDto

---
+++ frontend/src/dto/attribute.ts
@@ -0,0 +1,20 @@
+export class AttributeDto {
+    categories: Category;
+    name: string;
+    dataTypes: Type;
+}
+export enum Category {
+    Certification = 'CERTIFICATION',
+    ProfessionalKnowledge = 'PROFESSIONAL_KNOWLEDGE',
+    PersonalInformation = 'PERSONAL_INFORMATION',
+    SoftSkills = 'SOFT_SKILLS',
+}
+export enum Type {
+    String = 'STRING',
+    Markdown = 'MARKDOWN',
+    Image = 'IMAGE',
+    Number = 'NUMBER',
+    Date = 'DATE',
+    Period = 'PERIOD',
+    Boolean = 'BOOLEAN',
+}
