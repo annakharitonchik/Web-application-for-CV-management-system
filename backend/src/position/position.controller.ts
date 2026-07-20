e872b1d4077886cf45b0bac3268e1ccc0592f4ed
Anna <zharitonchik@icloud.com>
2026-07-20 18:52:24 +0300

    feat(position.controller,position.service): add crud logic

--- backend/src/position/position.controller.ts
+++ backend/src/position/position.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PositionService } from './position.service';
+import { PositionDto } from './dto/position.dto';
 
 @Controller('position')
 export class PositionController {
   constructor(private readonly positionService: PositionService) {}
+
+  @Post()
+  create(@Body() dto: PositionDto) {
+    return this.positionService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() dto: PositionDto) {
+    return this.positionService.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.positionService.delete(id);
+  }
+
+  @Get()
+  getAll() {
+    return this.positionService.getAll();
+  }
 }
--- backend/src/position/position.service.ts
+++ backend/src/position/position.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { PositionDto } from './dto/position.dto';
+import { Position } from '../../generated/prisma/client';
 
 @Injectable()
-export class PositionService {}
+export class PositionService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(dto: PositionDto): Promise<Position> {
+    const { name, description, isPublic, attributes } = dto;
+    return this.prismaService.position.create({
+      data: {
+        name: name.trim(),
+        description,
+        isPublic,
+        attributes: {
+          connect: attributes,
+        },
+      },
+    });
+  }
+
+  async update(id: number, dto: PositionDto) {
+    const { name, description, isPublic, attributes } = dto;
+    return this.prismaService.position.update({
+      where: { id },
+      data: {
+        name: name.trim(),
+        description,
+        isPublic,
+        attributes: {
+          connect: attributes,
+        },
+      },
+    });
+  }
+
+  async delete(id: number) {
+    return this.prismaService.position.delete({
+      where: { id },
+    });
+  }
+
+  async getAll() {
+    return this.prismaService.position.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+      include: {
+        attributes: true,
+      },
+    });
+  }
+}
