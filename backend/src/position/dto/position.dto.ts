82dca9837dafa7b5fdf9eeee974824fd290b9098
Anna <zharitonchik@icloud.com>
2026-07-21 20:05:09 +0300

    fix(position.dto.ts): change field attributes
    AttributeDto[] to string[]

--- backend/src/position/dto/position.dto.ts
+++ backend/src/position/dto/position.dto.ts
@@ -1,10 +1,9 @@
 import { IsNotEmpty } from 'class-validator';
-import { AttributeDto } from '../../attribute/dto/attribute.dto';
 
 export class PositionDto {
   @IsNotEmpty()
   name: string;
   description: string;
   isPublic: boolean;
-  attributes: AttributeDto[];
+  attributes: string[];
 }
