46ba9dba9fdec49711dcd437797e776aa5b6c259
Anna <zharitonchik@icloud.com>
2026-07-22 22:29:25 +0300

    feat(app): add auth to app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AttributeModule } from './attribute/attribute.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PositionModule } from './position/position.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, AttributeModule, PositionModule],
+  imports: [PrismaModule, AttributeModule, PositionModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
