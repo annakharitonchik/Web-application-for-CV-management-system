aa1117e2170fa246a245cf7bfbeb99ca6e87e280
Anna <zharitonchik@icloud.com>
2026-07-22 22:31:14 +0300

    feat(user.service.tc): remove password from function create user

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { User } from '../../generated/prisma/client';
-import { UserDto } from './dto/user.dto';
+import { AuthUserDto, UserDto } from './dto/user.dto';
 import { hashSync, genSaltSync } from 'bcrypt';
 @Injectable()
 export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
-  async create(dto: UserDto): Promise<User> {
+  async create(dto: UserDto): Promise<AuthUserDto> {
     const { email, password, role } = dto;
     const hashedPassword = this.hashPassword(password);
-    return this.prismaService.user.create({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...user } = await this.prismaService.user.create({
       data: {
         email,
         password: hashedPassword,
         role,
       },
     });
+
+    return user;
   }
   async findOne(email: string) {
     return this.prismaService.user.findFirst({
