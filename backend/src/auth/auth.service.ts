cb0529915074c112ae6357dff51c1ca2c5f3b66a
Anna <zharitonchik@icloud.com>
2026-07-23 18:10:23 +0300

    feat(generate token): generate access token

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,10 +2,15 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterRequest } from './dto/register.dto';
 import { genSaltSync, hashSync } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt.interfaces';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(dto: RegisterRequest) {
     const { email, password, role } = dto;
@@ -16,12 +21,22 @@ export class AuthService {
       throw new ConflictException('User exists');
     }
 
-    return this.prismaService.user.create({
+    const user = await this.prismaService.user.create({
       data: {
         email,
         password: hashSync(password, genSaltSync(10)),
         role,
       },
     });
+    return this.generateTokens(user.id);
+  }
+  private generateTokens(id: string) {
+    const payload: JwtPayload = { id };
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: '1d',
+    });
+    return {
+      accessToken,
+    };
   }
 }
