48db1f586b0dee4a27f5dae6c3f6f011d7d3433c
Anna <zharitonchik@icloud.com>
2026-07-23 17:44:43 +0300

    fix(auth): redo

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,22 +1,7 @@
-import { Controller, Get, Post, UseGuards, Req } from '@nestjs/common';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
-import { AuthUserDto } from './user/dto/user.dto';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  login(@Req() req: { user: AuthUserDto }) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Req() req: { user: AuthUserDto }) {
-    return req.user;
-  }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,32 +1,27 @@
-import { Injectable } from '@nestjs/common';
-import { UserService } from '../user/user.service';
-import { compareSync } from 'bcrypt';
-import { AuthUserDto } from '../user/dto/user.dto';
-import { JwtService } from '@nestjs/jwt';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterRequest } from './dto/register.dto';
+import { genSaltSync, hashSync } from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private usersService: UserService,
-    private jwtService: JwtService,
-  ) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-  async validateUser(
-    username: string,
-    pass: string,
-  ): Promise<AuthUserDto | null> {
-    const user = await this.usersService.findOne(username);
-    if (user && compareSync(pass, user.password)) {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { password, ...result } = user;
-      return result;
+  async register(dto: RegisterRequest) {
+    const { email, password, role } = dto;
+    const existUser = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (existUser) {
+      throw new ConflictException('User exists');
     }
-    return null;
-  }
-  login(user: AuthUserDto) {
-    const payload = { email: user.email, id: user.id };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+
+    return this.prismaService.user.create({
+      data: {
+        email,
+        password: hashSync(password, genSaltSync(10)),
+        role,
+      },
+    });
   }
 }
